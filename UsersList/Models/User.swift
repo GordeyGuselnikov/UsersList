93e5aae292ec21e913f0e4863847acafabe98b07
Guselnikov Gordey <guselnikovgordey@icloud.com>
2024-03-09 01:38:35 +0700

    настройки

--- UsersList/Models/User.swift
+++ UsersList/Models/User.swift
@@ -5,4 +5,6 @@
 //  Created by Gordey Guselnikov on 3/9/24.
 //
 
-import Foundation
+struct User {
+    
+}
--- UsersList/ViewControllers/UsersListViewController.swift
+++ UsersList/ViewControllers/UsersListViewController.swift
@@ -14,6 +14,6 @@ final class UsersListViewController: UIViewController {
         
     }
 
-
+    
 }
 
