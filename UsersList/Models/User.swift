576e0d3547f99c9636d456c517addcad9157e21e
Guselnikov Gordey <guselnikovgordey@icloud.com>
2024-03-09 11:22:40 +0700

    min working table with network request and model

--- UsersList/Models/User.swift
+++ UsersList/Models/User.swift
@@ -5,6 +5,25 @@
 //  Created by Gordey Guselnikov on 3/9/24.
 //
 
-struct User {
-    
+
+// MARK: - Query
+struct Query: Decodable {
+    let items: [User]
 }
+
+
+struct User: Decodable {
+    let id: String
+    let avatarUrl: String
+    let firstName: String
+    let lastName: String
+    let userTag: String
+//    let department: Departments
+    let position: String
+    let birthday: String
+    let phone: String
+}
+
+//enum Departments: String {
+//    
+//}
--- UsersList/ViewControllers/UsersListViewController.swift
+++ UsersList/ViewControllers/UsersListViewController.swift
@@ -9,11 +9,84 @@ import UIKit
 
 final class UsersListViewController: UIViewController {
 
+    let tableView = UITableView()
+    
+    private var users = [User]()
+    private let networkManager = NetworkManager.shared
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        setupTableView()
+        downloadDate()
+        
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    private func downloadDate() {
+        NetworkManager.shared.fetchUser { [weak self] result in
+            switch result {
+            case .success(let users):
+                print(users.count)
+                print(users)
+                //self?.showAlert(withStatus: .success)
+                self?.users = users
+                self?.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+//                self?.showAlert(withStatus: .failed)
+            }
+        }
+
+    }
+    
     
 }
+
+extension UsersListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            users.count
+        }
+    
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    
+            let user = users[indexPath.row]
+    
+            var content = cell.defaultContentConfiguration()
+            content.text = user.firstName
+            content.secondaryText = user.position
+//            content.text = "dsfdfsdfsdf"
+//            print(user.avatarUrl)
+            cell.contentConfiguration = content
+    
+            return cell
+        }
+    
+}
+
+
+    
+
+    
+    
+    
+
+
+
 
