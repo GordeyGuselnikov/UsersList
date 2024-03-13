2eb05317fa365162db7ec8742281ff739c7b3fac
Gordey Guselnikov <g.guselnikov@bi.zone>
2024-03-13 19:30:43 +0700

    fix fetch method and begin SearchBar

---
+++ UsersList/Views/UserCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserCell.swift
+//  UsersList
+//
+//  Created by Guselnikov Gordey on 13.03.24.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
